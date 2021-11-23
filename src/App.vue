d54dcc3291360467d174c55d34690accca8eabb9
Dan Popescu <pdan.popescu@gmail.com>
2021-11-23 18:43:22 +0200

    feat: change license and display in app

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,55 @@
     </q-page-container>
 
     <q-footer class="bg-primary">
-      <div class="row no-wrap items-center q-px-md q-py-xs">
-        <div class="text-caption">
-          {{ appAuthor }}
+      <div class="row no-wrap items-center q-px-md q-py-sm">
+        <div
+          xmlns:cc="http://creativecommons.org/ns#"
+          xmlns:dct="http://purl.org/dc/terms/"
+          class="row no-wrap items-center q-gutter-x-xs license text-subtitle1"
+        >
+          <a
+            href="https://github.com/pdanpdan/ssl-certificate-manager"
+            rel="cc:attributionURL"
+            property="dct:title"
+            target="_blank"
+          >SSL Certificate Manager</a>
+          <span>&copy;2021 by</span>
+          <a
+            href="https://github.com/pdanpdan/"
+            rel="cc:attributionURL dct:creator"
+            property="cc:attributionName"
+            target="_blank"
+          >Dan Popescu</a>
+          <span>is licensed under</span>
+          <a
+            class="row no-wrap items-center q-gutter-x-xs"
+            href="http://creativecommons.org/licenses/by-nc-nd/4.0/?ref=chooser-v1"
+            rel="license noopener noreferrer"
+            target="_blank"
+          >
+            <span>CC BY-NC-ND 4.0</span>
+            <img
+              class="license__img"
+              src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
+            />
+            <img
+              class="license__img"
+              src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"
+            />
+            <img
+              class="license__img"
+              src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"
+            />
+            <img
+              class="license__img"
+              src="https://mirrors.creativecommons.org/presskit/icons/nd.svg?ref=chooser-v1"
+            />
+          </a>
         </div>
 
         <q-space />
 
-        <div class="text-caption">
+        <div class="text-subtitle2">
           ver. {{ appVersion }}
         </div>
       </div>
